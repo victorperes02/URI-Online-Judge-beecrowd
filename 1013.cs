87846452e982aa0b07845e2ba4f7608c007be0ce
Victor Peres <72952664+victorperes02@users.noreply.github.com>
2022-08-15 16:30:38 -0300

    O Maior

---
+++ 1013.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace programa
+{
+    class Uri
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, s, maior1, maior2;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            s = int.Parse(Console.ReadLine());
+
+            maior1 = (a + b + Math.Abs(a - b)) / 2;
+
+            maior2 = (maior1 + s + Math.Abs(maior1 - s)) / 2;
+
+
+            Console.WriteLine($"{maior2} eh o maior");
+
+              Console.ReadKey();
+        }
+    }
+}
